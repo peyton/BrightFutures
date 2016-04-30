90fa778a0fb434db55f43a76ea3d2b74acea96a1
Thomas Visser <thomas.visser@gmail.com>
2016-04-30 16:03:24 +0200

    Improvements to package layout & SPM compatibility

--- Package.swift
+++ Package.swift
@@ -2,12 +2,18 @@ import PackageDescription
 
 let package = Package(
     name: "BrightFutures",
+    exclude: [
+        "BrightFutures.xcworkspace",
+        "Documentation",
+        "Carthage",
+        "BrightFuturesTests"
+    ],
     targets: [
         Target(
             name: "BrightFutures"
         ),
     ],
     dependencies: [
-    	.Package(url: "https://github.com/antitypical/Result.git", majorVersion: 2)
+    	.Package(url: "https://github.com/Thomvis/Result.git", majorVersion: 2)
     ]
 )
